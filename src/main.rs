addef0bfefb8c99d7da8e5e198cc6ffb6193af38
Renjith <grenjithr@gmail.com>
2021-01-20 00:32:04 -0600

    Press ctrl-q to quit

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,28 @@
-use std::io::{ self, Read };
+use std::io::{ self, stdout, Read };
+use termion::raw::IntoRawMode;
 
 fn main() {
+    let _stdout = stdout().into_raw_mode().unwrap();
+
     for b in io::stdin().bytes() {
-        let c = b.unwrap() as char;
-        println!("{}", c);
+        let b = b.unwrap();
+        let c = b as char;
+
+        println!("{:#b}", b);
+
+        if c.is_control() {
+            println!("{:?} \r", b);
+        } else {
+            println!("{:?} ({})\r", b, c);
+        }
+        
+        if b == to_ctrl_byte('q') {
+            break;
+        } 
     }
+}
+
+fn to_ctrl_byte(c: char) -> u8 {
+    let byte = c as u8;
+    byte & 0b0001_1111
 }
