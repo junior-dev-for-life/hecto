91de3855fe83cdd1e968dc0abdb052a003639f9b
Renjith <grenjithr@gmail.com>
2021-01-23 00:01:36 -0600

    Window size

--- src/editor.rs
+++ src/editor.rs
@@ -1,20 +1,21 @@
-use std::io::{ self, stdout, Write };
+use crate::terminal::Terminal;
 use termion::event::Key;
-use termion::input::TermRead;
-use termion::raw::IntoRawMode;
 
 pub struct Editor {
     should_quit: bool,
+    terminal: Terminal,
 }
 
 impl Editor {
     
     pub fn default() -> Self {
-        Self{ should_quit: false }
+        Self{
+            should_quit: false,
+            terminal: Terminal::default().expect("Failed to initialize terminal"),
+        }
     }
 
     pub fn run(&mut self) {
-        let _stdout = stdout().into_raw_mode().unwrap();
 
         loop {
             if let Err(error) = self.refresh_screen() {
@@ -30,34 +31,34 @@ impl Editor {
     }
 
     fn refresh_screen(&self) -> Result<(), std::io::Error> {
-        print!("{}{}", termion::clear::All, termion::cursor::Goto(1, 1));
+        Terminal::clear_screen();
+        Terminal::cursor_position(0, 0);
         if self.should_quit {
             println!("Goodbye. \r");
+        } else {
+            self.draw_rows();
+            Terminal::cursor_position(0, 0);
         }
-        io::stdout().flush()
+        Terminal::flush()
     }
 
     fn process_keypress(&mut self) -> Result<(), std::io::Error> {
-        let pressed_key = read_key()?;
         
-        if let Key::Ctrl('q') = pressed_key {
+        if let Key::Ctrl('q') = Terminal::read_key()? {
             self.should_quit = true;
         }
 
         Ok(())
     }
-}
 
-
-fn read_key() -> Result<Key, std::io::Error> {
-    loop {
-        if let Some(key) = io::stdin().lock().keys().next() {
-            return key;
-        } 
+    fn draw_rows(&self) {
+        for _ in 0..self.terminal.size().height {
+            println!("~\r");
+        }
     }
 }
 
 fn die(e: std::io::Error) {
-    print!("{}", termion::clear::All);
+    Terminal::clear_screen();
     panic!(e);
 }
