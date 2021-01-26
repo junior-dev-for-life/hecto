a4b681b8d0b985eef10db0643d7a3da05bff3663
Renjith <grenjithr@gmail.com>
2021-01-26 16:28:48 -0600

    A line viewer

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 #![warn(clippy::all, clippy::pedantic)]
 mod editor;
 mod terminal;
-use editor::Editor;
+mod row;
+mod document;
 pub use editor::Position;
+pub use terminal::Terminal;
+pub use document::Document;
+pub use row::Row;
+use editor::Editor;
 
 fn main() {
     Editor::default().run();
